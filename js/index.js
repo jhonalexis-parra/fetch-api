b4456d451136f3f43c902d81cc6082e7ee5f7b1e
jhonalexis-parra <jhalpaab@gmail.com>
2021-07-07 15:06:50 -0500

    [ADD] Ejercicio finalizado

--- js/index.js
+++ js/index.js
@@ -5,6 +5,7 @@ fetch('https://randomuser.me/api/')
     })
     .then(data => {
         //manipulamos la respuesta
+        renderizarDatosUsuario(data.results[0]);
         console.log(data)
     });
 
@@ -13,15 +14,35 @@ function renderizarDatosUsuario(datos) {
     // Aquí deben desarrollar una función que muestre en pantalla:
     // la foto, el nombre completo del usuario y su email.
     // Esto debe estar basado en la info que nos llega desde la API e insertarse en el HTML.
+    console.log(datos);
+
+    contenedor = document.querySelector(".tarjeta"); 
     
+    template = 
+    ` <img src="${datos.picture.medium}" alt="${datos.imagenPerfilMedium}" ><br> 
+    <p class="nombre">Nombre Completo: ${datos.name.title} ${datos.name.first} ${datos.name.last}</p><br> 
+    <p class="email">Email: ${datos.email}</p> 
+    `; 
     
-    
-    
+    contenedor.innerHTML = template;
     
 }
-
 
 /* --------------------------- CONSIGNA 2 (extra) --------------------------- */
 // Aqui pueden ir por el punto extra de utilizar el boton que se encuentra comentado en el HTML
 // Pueden descomentar el código del index.html y usar ese boton para ejecutar un nuevo pedido a la API, sin necesidad de recargar la página.
 // Es criterio del equipo QUÉ bloque del código debe contenerse dentro de una función para poder ser ejecutada cada vez que se escuche un click.
+
+window.onload = () => {
+    const boton = document.querySelector(".btnContainer").addEventListener("click", () => {
+        fetch('https://randomuser.me/api/')
+    .then(response => {
+        return response.json()
+    })
+    .then(data => {
+        //manipulamos la respuesta
+        renderizarDatosUsuario(data.results[0]);
+        console.log(data)
+    });
+    });
+}
